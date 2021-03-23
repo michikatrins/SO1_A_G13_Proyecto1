864ed3e1a7d6eda278674618713e60e4a1dd441b
Cristian Azurdia <1983964490101@ingenieria.usac.edu.gt>
2021-03-22 20:43:42 -0600

    version 3.0
    Publisher:
    -Levanta Servicio en puerto 90
    -prueba localhost finalizada
    -recibe json formato enunciado

--- Google PubSub/Publisher/publisherSO1.go
+++ Google PubSub/Publisher/publisherSO1.go
@@ -2,16 +2,59 @@ package main
 
 import (
 	"context"
-
+	"encoding/json"
 	"fmt"
+
+	"net/http"
 
 	"cloud.google.com/go/pubsub"
 )
 
-func publish(msg string) error {
+type msg_COVID struct {
+	Name         string
+	Location     string
+	Age          int
+	Infectedtype string
+	State        string
+}
 
-	projectId := "so1-1s2021-primerproyecto"
-	topicId := "SO1_Chat"
+const (
+	projectId  = "so1-1s2021-primerproyecto"
+	topicId    = "SO1_Chat"
+	portServer = 80
+)
+
+func Server(wr http.ResponseWriter, req *http.Request) {
+
+	switch req.Method {
+	case "POST":
+		var json_covid msg_COVID
+
+		//route := "Google pub/sub"
+		decoder := json.NewDecoder(req.Body)
+		_error := decoder.Decode(&json_covid)
+
+		if _error != nil {
+			fmt.Println("Error al recibir cadena: %v", _error)
+		}
+
+		message, _error := json.Marshal(msg_COVID{Name: json_covid.Name, Location: json_covid.Location, Age: json_covid.Age, Infectedtype: json_covid.Infectedtype, State: json_covid.State})
+
+		if _error != nil {
+			fmt.Fprintf(wr, "Error en parser: %v", _error)
+			return
+		}
+
+		fmt.Println(string(message))
+		publish(string(message))
+
+	default:
+		fmt.Println(wr, "Ruta %s no validada", req.Method)
+		return
+	}
+}
+
+func publish(msg string) error {
 
 	ctx := context.Background()
 
@@ -39,7 +82,12 @@ func main() {
 	fmt.Println("Iniciando ...")
 	fmt.Println("..")
 	fmt.Println(".")
-	publish("Hola a todos")
+	http.HandleFunc("/", Server)
+	puerto := ":80"
+	_error := http.ListenAndServe(puerto, nil)
+	if _error != nil {
+		fmt.Println("Error en el WebService: %v", _error)
+	}
 }
 
 //windows
