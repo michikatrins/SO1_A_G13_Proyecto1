24addc80b9d2093720ad1408bede427146d1e415
michikatrins <michikatrins@gmail.com>
2021-03-24 23:43:47 -0600

    cambio :)

--- React/src/views/Reporte10.js
+++ React/src/views/Reporte10.js
@@ -1,69 +1,79 @@
 import React, { Component } from 'react';
+import CanvasJSReact from '../assets/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-
-
-
-import {
-  Route,
-  NavLink,
-  BrowserRouter
-} from "react-router-dom";
-
-import './bootstrap.min.css'; 
-import DynamicRam from './dynamic charts/Dynamic Ram';
-import BootstrapTable from 'react-bootstrap-table-next';
-
-const person = [
-    {id: 1, name: 'Gob', value: '2'},
-    {id: 2, name: 'Buster', value: '5'},
-    {id: 3, name: 'George Michael', value: '4'}
-  ];
-  const columns = [{
-    dataField: 'id',
-    text: 'PID'
-  },
-  {
-    dataField: 'name',
-    text: 'Name'
-  },
-  {
-    dataField: 'value',
-    text: 'Father PID'
-  },
-  {
-    dataField: 'value',
-    text: 'Status'
-  }];
-
-
+var dps = [{x: 1, y: 10}, {x: 2, y: 13}, {x: 3, y: 18}, {x: 4, y: 20}, {x: 5, y: 17},{x: 6, y: 10}, {x: 7, y: 13}, {x: 8, y: 18}, {x: 9, y: 20}, {x: 10, y: 17}];   //dataPoints.
+var xVal = dps.length + 1;
+var yVal = 15;
+var updateInterval = 1000;
 class Reporte10 extends Component {  
-  render() {    
-    return (
-		<div>
-			<Navbar bg="dark" variant="dark">
-				<button className="d-lg-none toggle-sidebar"><span className="navbar-toggler-icon"></span></button>
-				<Navbar.Brand href="/">System Monitor</Navbar.Brand><span className="hidden-xs text-muted">Grupo_13</span>
-			  </Navbar>		  
-			  <BrowserRouter>		  
-					<Row>
-						<Col xl={{ span: 7, offset: 3 }} lg={{ span: 8, offset: 3 }} xs={{ span: 8, offset: 2 }}>
-							<Container>
-								<div className="content">
-									<Route exact path="/" component={DynamicRam}/>
-								</div>
-							</Container>
-						</Col>					
-					</Row>			
-			  </BrowserRouter>
-		</div>
-    );
-  }
+  
+	state = {
+    file: [],
+    range: [],
+};
+
+constructor() {
+super();
+this.updateChart = this.updateChart.bind(this);
+}
+
+// componentDidMount() {
+// 	setInterval(this.updateChart, updateInterval);
+// }
+
+componentDidMount() {
+fetch("http://34.69.47.240/mem",)
+  .then((response) => response.json())
+  .then((datos) => this.setState({file : datos}))
+  setInterval(this.updateChart, updateInterval);
+}
+
+updateChart() {
+console.log(this.state.file);
+//yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
+yVal = this.state.file['PorcentajeConsumo'];
+dps.push({x: xVal,y: yVal});
+xVal++;
+if (dps.length >  10 ) {
+  dps.shift();
+}
+this.chart.render();
+}
+
+render() {
+const options = {
+  theme: "light3",
+  exportEnabled: true,
+  title :{
+    text: "RAM"
+  },
+  axisX: {
+    title: "Segundos",
+    suffix: "s"
+  },
+        axisY: {
+    title: "Porcentaje de Uso",
+    suffix: "%"
+  },
+  data: [{
+    type: "area",
+    dataPoints : dps
+  }]
+}
+
+return (
+<div>
+        
+  <h1>CONSUMO DE MEMORIA</h1>
+        
+  { <CanvasJSChart options = {options} 
+    onRef={ref => this.chart = ref}
+  />}
+  {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+</div>
+);
+}
 }
 
 export default Reporte10;
--- React/src/views/Reporte7.js
+++ React/src/views/Reporte7.js
@@ -1,70 +1,124 @@
 import React, { Component } from 'react';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-
-
-
-import {
-  Route,
-  NavLink,
-  BrowserRouter
-} from "react-router-dom";
-
-import './bootstrap.min.css'; 
-import DynamicRam from './dynamic charts/Dynamic Ram';
-import LineChart from './column charts/Ages Chart';
-import BootstrapTable from 'react-bootstrap-table-next';
-
-const person = [
-    {id: 1, name: 'Gob', value: '2'},
-    {id: 2, name: 'Buster', value: '5'},
-    {id: 3, name: 'George Michael', value: '4'}
-  ];
-  const columns = [{
-    dataField: 'id',
-    text: 'PID'
-  },
-  {
-    dataField: 'name',
-    text: 'Name'
-  },
-  {
-    dataField: 'value',
-    text: 'Father PID'
-  },
-  {
-    dataField: 'value',
-    text: 'Status'
-  }];
+import CanvasJSReact from '../assets/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
 class Reporte7 extends Component {  
-  render() {    
-    return (
-		<div>
-			<Navbar bg="dark" variant="dark">
-				<button className="d-lg-none toggle-sidebar"><span className="navbar-toggler-icon"></span></button>
-				<Navbar.Brand href="/">System Monitor</Navbar.Brand><span className="hidden-xs text-muted">Grupo_13</span>
-			  </Navbar>		  
-			  <BrowserRouter>		  
-					<Row>
-						<Col xl={{ span: 7, offset: 3 }} lg={{ span: 8, offset: 3 }} xs={{ span: 8, offset: 2 }}>
-							<Container>
-								<div className="content">
-									<Route exact path="/" component={LineChart}/>
-								</div>
-							</Container>
-						</Col>					
-					</Row>			
-			  </BrowserRouter>
-		</div>
-    );
+  
+  state = {
+    file: [],
+    range: [],
+};
+
+componentDidMount() {
+fetch("http://34.69.47.240/data",)
+  .then((response) => response.json())
+  .then((datos) => this.setState({file : datos}))
+}
+
+render() {
+   
+    console.log(this.state.file);
+
+    for(let x = 0; x < 10; x++)
+    {
+        this.state.range[x] = 0;
+    }
+
+    for(let y = 0; y < this.state.file.length; y++)
+    {
+        let e = this.state.file[y]['age'];
+        if(e != undefined)
+        {
+            if(e >0 && e <10)
+            {
+                this.state.range[0] = this.state.range[0] + 1;
+            }
+            else if(e >= 10 && e < 20)
+            {
+                this.state.range[1] = this.state.range[1] + 1;
+            }
+            else if(e >= 20 && e < 30)
+            {
+                this.state.range[2] = this.state.range[2] + 1;
+            }
+            else if(e >= 30 && e < 40)
+            {
+                this.state.range[3] = this.state.range[3] + 1;
+            }
+            else if(e >= 40 && e < 50)
+            {
+                this.state.range[4] = this.state.range[4] + 1;
+            }
+            else if(e >= 50 && e < 60)
+            {
+                this.state.range[5] = this.state.range[5] + 1;
+            }
+            else if(e >= 60 && e < 70)
+            {   
+                this.state.range[6] = this.state.range[6] + 1;
+            }
+            else if(e >= 70 && e < 80)
+            {
+                this.state.range[7] = this.state.range[7] + 1;
+            }
+            else if(e >= 80 && e < 90)
+            {
+                this.state.range[8] = this.state.range[8] + 1;
+            }
+            else if(e >= 90 && e < 100)
+            {
+                this.state.range[9] = this.state.range[9] + 1;
+            }
+        }
+    }
+
+    console.log(this.state.range);
+    
+const options = {
+        exportEnabled: true,
+  animationEnabled: true,
+  title: {
+    text: "Personas Infectadas"
+  },
+        axisX: {
+    title: "Rango de Edades",
+    suffix: ""
+  },
+        axisY: {
+    title: "Cantidad Infectados",
+    suffix: ""
+  },
+  data: [
+  {
+    // Change type to "doughnut", "line", "splineArea", etc.
+    type: "column",
+    dataPoints: [
+      { label: "0-10",  y:  this.state.range[0] },
+      { label: "10-20", y:  this.state.range[1] },
+      { label: "20-30", y:  this.state.range[2] },
+      { label: "30-40", y:  this.state.range[3] },
+      { label: "40-50", y:  this.state.range[4] },
+                { label: "50-60", y:  this.state.range[5] },
+      { label: "60-70", y:  this.state.range[6] },
+      { label: "70-80", y:  this.state.range[7] },
+      { label: "80-90", y:  this.state.range[8] },
+      { label: "90-100", y: this.state.range[9] },
+    ]
   }
+  ]
+}
+
+return (
+<div>
+  <h1>Rangos de Edad</h1>
+  <CanvasJSChart options = {options} 
+    /* onRef={ref => this.chart = ref} */
+  />
+  {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+</div>
+);
+}
 }
 
 export default Reporte7;
